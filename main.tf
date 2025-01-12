26b6e501e726635d38db5f740ba53de411f73181
Waddenn <tpatelas@proton.me>
2025-01-12 11:49:08 +0100

    fix: values

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
 # main.tf
 
-module "lxc_a" {
+module "tailscale-subnet" {
   source = "./modules/lxc"
 
-  lxc_hostname      = "test-lxc-a"
+  lxc_hostname      = "tailscale-subnet"
   target_node       = "proxade"
   lxc_cores         = 1
-  lxc_memory        = 512
+  lxc_memory        = 128
   lxc_password      = var.lxc_password_a
   unprivileged      = true
-  lxc_rootfs_size   = "8G"
+  lxc_rootfs_size   = "4G"
   network_ip        = "192.168.1.201"
   network_gateway = "192.168.1.254"
   features_nesting  = true
 
 }
 
-module "lxc_b" {
+module "tailscale-exit-node" {
   source = "./modules/lxc"
 
   lxc_hostname      = "test-lxc-b"
   target_node       = "proxade"
-  lxc_cores         = 2
-  lxc_memory        = 1024
+  lxc_cores         = 1
+  lxc_memory        = 128
   lxc_password      = var.lxc_password_b
   unprivileged      = true
-  lxc_rootfs_size   = "16G"
+  lxc_rootfs_size   = "4G"
   network_ip        = "192.168.1.202"
   network_gateway = "192.168.1.254"
   features_nesting  = true
