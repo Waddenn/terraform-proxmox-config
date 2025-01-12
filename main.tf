7c211251a57e40afb0d36e25d00b0bf159123d82
waddenn <waddenn.github@gmail.com>
2025-01-12 21:50:03 +0100

    update

--- main.tf
+++ main.tf
@@ -104,9 +104,9 @@ module "nixos-test" {
   target_node       = "proxade"
   lxc_cores         = 1
   lxc_memory        = 512
-  lxc_password      = var.lxc_password_d
+  lxc_password      = null
   unprivileged      = true
-  lxc_ostemplate   = "local:vztmpl/nixos-system-linux.tar.xz"
+  lxc_ostemplate   = "local:vztmpl/nixos-system-x86_64-linux.tar.xz"
   lxc_rootfs_size   = "4G"
   network_ip        = "192.168.1.206/24"
   network_gateway = "192.168.1.254"
