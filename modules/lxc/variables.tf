98050a3ecca6a9e8de02b8e11b51d6e31932506d
waddenn <waddenn.github@gmail.com>
2025-01-12 20:26:06 +0100

    fix: console mode

--- modules/lxc/variables.tf
+++ modules/lxc/variables.tf
@@ -100,5 +100,4 @@ variable "ssh_public_key" {
 variable "console_mode" {
   type        = string
   description = "Mode de console pour le conteneur (ex: tty ou /dev/console)."
-  default     = "/dev/console"
 }
