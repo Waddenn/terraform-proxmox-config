7baefbf3d3bb57ccf96c50a41090cf51a8fa54e7
waddenn <waddenn.github@gmail.com>
2025-01-12 21:24:43 +0100

    test

--- main.tf
+++ main.tf
@@ -76,25 +76,25 @@ module "ansible" {
   EOT
 }
 
-module "web_server" {
-  source = "./modules/lxc_clone"
+# module "web_server" {
+#   source = "./modules/lxc_clone"
 
-  lxc_vmid         = 205
-  lxc_hostname     = "web-server"
-  target_node      = "proxade"
-  template_id      = "298"
-  lxc_cores        = 4
-  lxc_memory       = 1024
-  lxc_password     = var.lxc_password_d
-  unprivileged     = true
-  lxc_storage      = "local-lvm"
-  lxc_rootfs_size  = "10G"
-  network_ip       = "192.168.1.205/24"
-  network_gateway  = "192.168.1.254"
-  ssh_public_key    = <<-EOT
-    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCKjyasY2YEtzqecWUQY2/AeTb7LEauFDDatzByT1P0WvQKVdpyZ///i8Y6qv9OuMhgrDvvHZK3mne7l/ecaYuc3gvsgFDpu2FXAFMWmRnb/+xW41qxHKI927KcGr0xGCWKO03cgxzq5k0gUmqHbQR1Q9aT4KiEJ9qc4K11zW2FI543IlpfPIUYSeXXOC0cj2MaQwhQpU3AOEblPOvkWz80OyW3lvRPADgSmzrKTNkd4iDUNbRsI4jB8HWJq2W3nGjiU9WCv12SU94tQRt015oxqae6Kww3gTnoUjeY6unOjZiXN7NCVmc+YFSyCKwafpl5Yk+kUSSGVb1dsuvNmYxp6j1112/yTUXl1scZqgmbHc6ODyGhMgDkuPqzW0IgulHoffn3OUuOzoyU4CIYLpZ+Qb9wE1tRbEifHTpK6it76ttA3C8QJIiWehstV3xJnM3C0Ki4DKvmllv0o55Z0jRBCqDq6sGJ51whel/GBHP4Ddfp/u2452GZ5HxjZJFdY8LqFJp3oS3RfprCJBSt1EUfILziiGj9APWKJGvmYh4qYqnxPE8pHJlh7VjcH1w3qVFGQYJ3DEGKcehATj2HEFgVknRniO0Q2uk8p4L1Em5u1VfRiVXojDKo1DL4HiFvtnF5fqWKCjCsctZtnfqRcz6br5tDlHaNU9vHhtBhwGrPpw== tom@asus-nixos
-  EOT
-}
+#   lxc_vmid         = 205
+#   lxc_hostname     = "web-server"
+#   target_node      = "proxade"
+#   template_id      = "298"
+#   lxc_cores        = 4
+#   lxc_memory       = 1024
+#   lxc_password     = var.lxc_password_d
+#   unprivileged     = true
+#   lxc_storage      = "local-lvm"
+#   lxc_rootfs_size  = "10G"
+#   network_ip       = "192.168.1.205/24"
+#   network_gateway  = "192.168.1.254"
+#   ssh_public_key    = <<-EOT
+#     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCKjyasY2YEtzqecWUQY2/AeTb7LEauFDDatzByT1P0WvQKVdpyZ///i8Y6qv9OuMhgrDvvHZK3mne7l/ecaYuc3gvsgFDpu2FXAFMWmRnb/+xW41qxHKI927KcGr0xGCWKO03cgxzq5k0gUmqHbQR1Q9aT4KiEJ9qc4K11zW2FI543IlpfPIUYSeXXOC0cj2MaQwhQpU3AOEblPOvkWz80OyW3lvRPADgSmzrKTNkd4iDUNbRsI4jB8HWJq2W3nGjiU9WCv12SU94tQRt015oxqae6Kww3gTnoUjeY6unOjZiXN7NCVmc+YFSyCKwafpl5Yk+kUSSGVb1dsuvNmYxp6j1112/yTUXl1scZqgmbHc6ODyGhMgDkuPqzW0IgulHoffn3OUuOzoyU4CIYLpZ+Qb9wE1tRbEifHTpK6it76ttA3C8QJIiWehstV3xJnM3C0Ki4DKvmllv0o55Z0jRBCqDq6sGJ51whel/GBHP4Ddfp/u2452GZ5HxjZJFdY8LqFJp3oS3RfprCJBSt1EUfILziiGj9APWKJGvmYh4qYqnxPE8pHJlh7VjcH1w3qVFGQYJ3DEGKcehATj2HEFgVknRniO0Q2uk8p4L1Em5u1VfRiVXojDKo1DL4HiFvtnF5fqWKCjCsctZtnfqRcz6br5tDlHaNU9vHhtBhwGrPpw== tom@asus-nixos
+#   EOT
+# }
 
 module "nixos-test" {
   source = "./modules/lxc"
