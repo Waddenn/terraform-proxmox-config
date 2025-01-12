656c52e78a2ad3497d0d54084d03e596883d3471
waddenn <waddenn.github@gmail.com>
2025-01-12 22:09:24 +0100

    update: ostemplate

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ module "nixos-test" {
   lxc_memory        = 512
   lxc_password      = null
   unprivileged      = true
-  lxc_ostemplate   = "local:vztmpl/nixos-system-x86_64-linux.tar.xz"
+  lxc_ostemplate   = "local:vztmpl/nixos-image-lxc-proxmox-25.05beta736170.130595eba610-x86_64-linux.tar.xz"
   lxc_rootfs_size   = "4G"
   network_ip        = "192.168.1.206/24"
   network_gateway = "192.168.1.254"
