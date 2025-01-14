bcc76fc262ee822b4e57f22820d0d963bbfc4484
waddenn <waddenn.github@gmail.com>
2025-01-14 12:24:48 +0100

    feat: new module

--- main.tf
+++ main.tf
@@ -168,22 +168,21 @@ module "adguardhome" {
 }
 
 module "searxng" {
-  source = "./modules/lxc"
-
-  lxc_vmid         = 209
+  source            = "./modules/lxc_custom"
+  lxc_vmid          = 209
   lxc_hostname      = "searxng"
   target_node       = "proxade"
   lxc_cores         = 2
   lxc_memory        = 1024
   lxc_password      = null
   unprivileged      = true
-  lxc_ostemplate   = "local:vztmpl/nixos-image-lxc-proxmox-25.05beta-x86_64-linux.tar.xz"
+  lxc_ostemplate    = "local:vztmpl/nixos-image-lxc-proxmox-25.05beta-x86_64-linux.tar.xz"
   lxc_rootfs_size   = "8G"
   network_ip        = "192.168.1.209/24"
-  network_gateway = "192.168.1.254"
+  network_gateway   = "192.168.1.254"
   features_nesting  = true
-  console_mode     = "console"
-  ostype               = "nixos"
+  console_mode      = "console"
+  ostype            = "nixos"
   ssh_public_key    = <<-EOT
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCKjyasY2YEtzqecWUQY2/AeTb7LEauFDDatzByT1P0WvQKVdpyZ///i8Y6qv9OuMhgrDvvHZK3mne7l/ecaYuc3gvsgFDpu2FXAFMWmRnb/+xW41qxHKI927KcGr0xGCWKO03cgxzq5k0gUmqHbQR1Q9aT4KiEJ9qc4K11zW2FI543IlpfPIUYSeXXOC0cj2MaQwhQpU3AOEblPOvkWz80OyW3lvRPADgSmzrKTNkd4iDUNbRsI4jB8HWJq2W3nGjiU9WCv12SU94tQRt015oxqae6Kww3gTnoUjeY6unOjZiXN7NCVmc+YFSyCKwafpl5Yk+kUSSGVb1dsuvNmYxp6j1112/yTUXl1scZqgmbHc6ODyGhMgDkuPqzW0IgulHoffn3OUuOzoyU4CIYLpZ+Qb9wE1tRbEifHTpK6it76ttA3C8QJIiWehstV3xJnM3C0Ki4DKvmllv0o55Z0jRBCqDq6sGJ51whel/GBHP4Ddfp/u2452GZ5HxjZJFdY8LqFJp3oS3RfprCJBSt1EUfILziiGj9APWKJGvmYh4qYqnxPE8pHJlh7VjcH1w3qVFGQYJ3DEGKcehATj2HEFgVknRniO0Q2uk8p4L1Em5u1VfRiVXojDKo1DL4HiFvtnF5fqWKCjCsctZtnfqRcz6br5tDlHaNU9vHhtBhwGrPpw== tom@asus-nixos
   EOT
