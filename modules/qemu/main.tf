fd406346aab68bfacb2a467bcf0501a4f6bf307d
Waddenn <tpatelas@proton.me>
2025-01-17 11:20:52 +0100

    fix: qemu vm

--- modules/qemu/main.tf
+++ modules/qemu/main.tf
@@ -12,12 +12,12 @@ resource "proxmox_vm_qemu" "vm" {
   memory      = var.memory
   bios        = var.bios
   onboot      = var.start_on_boot
-  oncreate    = var.start_on_create
+  start       = var.start_on_create  # Correction: oncreate -> start
 
   # Hardware Configuration
   scsihw      = "virtio-scsi-pci"
   boot        = "order=scsi0;ide2"
-  agent       = var.qemu_agent
+  agent       = var.qemu_agent ? 1 : 0  # Correction: bool -> number
 
   # Network configuration
   network {
