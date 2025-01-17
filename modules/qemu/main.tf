acb9e76e24d3c89f9d92a3fcecaf726590d25351
waddenn <waddenn.github@gmail.com>
2025-01-17 12:53:38 +0100

    remove some variables

--- modules/qemu/main.tf
+++ modules/qemu/main.tf
@@ -11,12 +11,6 @@ resource "proxmox_vm_qemu" "vm" {
   memory      = var.memory
   bios        = var.bios
   onboot      = var.start_on_boot
-#   start       = var.start_on_create  # Correction: oncreate -> start
-
-  # Hardware Configuration
-  scsihw      = "virtio-scsi-pci"
-  boot        = "order=scsi0;ide2"
-  agent       = var.qemu_agent ? 1 : 0  # Correction: bool -> number
 
   # Network configuration
   network {
@@ -26,13 +20,11 @@ resource "proxmox_vm_qemu" "vm" {
     tag      = 0
     id      = 0
   }
-   os_type    = "l26"
   # Disk configuration
   disk {
     storage = var.disk_storage
     size    = var.disk_size
     format  = var.disk_format
-    slot    = 0   
        
   }
    clone = 1002
