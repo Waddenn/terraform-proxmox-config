36e68f165684e56a162c37385b352f3478ce245c
Waddenn <tpatelas@proton.me>
2025-02-16 15:34:33 +0100

    feat: uncomment caddy configuration in LXC container setup

--- main.tf
+++ main.tf
@@ -68,15 +68,15 @@ module "lxc_containers" {
       }
     }
 
-    # caddy = {
-    #   vmid         = 105
-    #   target_node  = "nuc-pve-1"
-    #   rootfs_storage = "local-lvm"
-    #   ostemplate   = "local:vztmpl/nixos-image-lxc-base-proxmox-25.05-x86_64-linux.tar.xz"
-    #   network = {
-    #     ip       = "192.168.1.105/24"
-    #   }
-    # }
+    caddy = {
+      vmid         = 105
+      target_node  = "nuc-pve-1"
+      rootfs_storage = "local-lvm"
+      ostemplate   = "local:vztmpl/nixos-image-lxc-base-proxmox-25.05-x86_64-linux.tar.xz"
+      network = {
+        ip       = "192.168.1.105/24"
+      }
+    }
 
     nextcloud = {
       vmid         = 106
