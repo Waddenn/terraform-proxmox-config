37afad0e4084dd287797bdacd7fa6d2c664f534a
waddenn <waddenn.github@gmail.com>
2025-02-17 16:31:55 +0100

    feat: update ostemplate for LXC container configuration

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "lxc_containers" {
       vmid         = 103
       target_node  = "nuc-pve-1"
       rootfs_storage = "local-lvm"
-      ostemplate   = "local:vztmpl/nixos-image-lxc-docker-proxmox-25.05-x86_64-linux.tar.xz"
+      ostemplate   = "local:vztmpl/nixos-image-lxc-base-proxmox-25.05-x86_64-linux.tar.xz"
       network = {
         ip       = "192.168.1.103/24"
       }
