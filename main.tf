1047124a3fd928b9b2e3f34502766e6aef741066
Waddenn <tpatelas@proton.me>
2025-03-10 11:30:55 +0100

    feat: add memory configuration for LXC container

--- main.tf
+++ main.tf
@@ -87,6 +87,7 @@ module "lxc_containers" {
       target_node  = "nuc-pve-1"
       rootfs_storage = "local-lvm"
       rootfs_size = "16G"
+      memory = 6144
       ostemplate   = "local:vztmpl/nixos-image-lxc-base-proxmox-25.05-x86_64-linux.tar.xz"
       network = {
         ip       = "192.168.1.104/24"
