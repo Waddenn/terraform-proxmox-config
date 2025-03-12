0d58bb2a4fd59956c21e48b303bd89548cfc903f
waddenn <waddenn.github@gmail.com>
2025-03-12 12:34:15 +0100

    feat: add configuration for Grafana LXC container with network settings

--- main.tf
+++ main.tf
@@ -236,5 +236,16 @@ module "lxc_containers" {
         gateway  = "192.168.40.254"
       }
     }
+
+    grafana-1 = {
+      vmid         = 117
+      target_node  = "nuc-pve-1"
+      rootfs_storage = "local-lvm"
+      ostemplate   = "local:vztmpl/nixos-image-lxc-base-proxmox-25.05-x86_64-linux.tar.xz"
+      network = {
+        ip       = "192.168.1.117/24"
+    } 
+    }
+  
 }
 }
