f9cacd1acf3c48b2c03449e13dded61db798b65c
waddenn <waddenn.github@gmail.com>
2025-04-15 20:09:53 +0200

    feat: add bourse-dashboard LXC container configuration with network settings

--- main.tf
+++ main.tf
@@ -236,6 +236,16 @@ module "lxc_containers" {
         gateway  = "192.168.40.254"
       }
      }
+
+    bourse-dashboard = {
+      vmid         = 119
+      target_node  = "nuc-pve-1"
+      rootfs_storage = "local-lvm"
+      ostemplate   = "local:vztmpl/nixos-image-lxc-base-proxmox-25.05-x86_64-linux.tar.xz"
+      network = {
+        ip       = "192.168.1.119/24"
+      }
+     }
      
 
     nixos-gui = {
