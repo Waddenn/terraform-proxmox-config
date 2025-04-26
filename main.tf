5f0d8dbb86fbf7a4c05b2a769c621407cf9718df
Waddenn <tpatelas@proton.me>
2025-04-26 20:18:55 +0200

    feat: add rootfs_size for new LXC container configuration

--- main.tf
+++ main.tf
@@ -214,6 +214,7 @@ module "lxc_containers" {
       vmid         = 117
       target_node  = "nuc-pve-1"
       rootfs_storage = "local-lvm"
+      rootfs_size = "16G"
       ostemplate   = "local:vztmpl/nixos-image-lxc-base-proxmox-25.05-x86_64-linux.tar.xz"
       network = {
         ip       = "192.168.1.117/24"
