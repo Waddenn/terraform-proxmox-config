2a4db00138b8a2e4f76b31c5e70dc55c3297013f
Waddenn <tpatelas@proton.me>
2025-05-25 18:03:25 +0200

    feat: add glance container configuration in main.tf

--- main.tf
+++ main.tf
@@ -265,6 +265,16 @@ module "lxc_containers" {
         gateway  = "192.168.40.254"
       }
      }
+
+    glance = {
+      vmid         = 122
+      target_node  = "nuc-pve-1"
+      rootfs_storage = "local-lvm"
+      ostemplate   = "local:vztmpl/nixos-image-lxc-base-proxmox-25.05-x86_64-linux.tar.xz"
+      network = {
+        ip       = "192.168.1.122/24"
+     }
+    }
     
     github-runner = {
       vmid         = 251
